6bf38aaacf649427c196f0884406e4437f937fe7
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-04-27 22:07:41 +0300

    fixed bug with boundaries

--- GameJava/src/model/Model.java
+++ GameJava/src/model/Model.java
@@ -94,8 +94,8 @@ public class Model {
         boolean isEqual = checkNumber == rightNumber;
         if (!isEqual) {
             if (checkNumber < rightNumber)
-                addRangeToStatistics(checkNumber, maxValues.get(maxValues.size() - 1));
-            else addRangeToStatistics(minValues.get(minValues.size() - 1), checkNumber);
+                addRangeToStatistics(checkNumber + 1, maxValues.get(maxValues.size() - 1));
+            else addRangeToStatistics(minValues.get(minValues.size() - 1), checkNumber - 1);
         }
         return isEqual;
     }
